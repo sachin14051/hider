04771d485c442197727505a986cad5722cb6a028
sachin <sachin14051@gmail.com>
2023-08-03 00:35:57 +0530

    working on ngstyle&ngif

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-public product : string = "iphone";
+public product : string = "trip tickets";
 backgroundColor = "orange"
 public isAvailiable !:boolean;
 public skills : Array<string> = ["html", "css" , "javascript" ,"flex"];
