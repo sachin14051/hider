8be1fdd034ac703c76ce95618315c06132fbff4f
sachin <sachin14051@gmail.com>
2023-08-03 00:23:02 +0530

    working on ngstyle and ngfor

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+public product : string = "iphone";
+backgroundColor = "orange"
+public isAvailiable !:boolean;
+public skills : Array<string> = ["html", "css" , "javascript" ,"flex"];
+public frameworks : Array<string> = [ "angular","bootstrap","react"];
+public knoweledge !: Array<string>;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isAvailiable = Math.random() > 0.5 ? true : false ;
+    this.knoweledge = Math.random()>0.5 ? this.skills : this.frameworks
+  }
+
+}
